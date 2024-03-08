b55b92d0b4921b9ce636ca5baef93e0be23b3510
kaeru2193 <kaeru2193@gmail.com>
2024-03-08 20:04:39 +0900

    add: ogp settings.

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,7 @@
 insertTemplate("header", "header")
 insertTemplate("stub", "stub")
 insertTemplate("footer", "footer")
-setTitle()
+setHead()
 document.body.style.display = 'block' //読み込みが終わったら表示
 
 window.onload = () => {
@@ -32,9 +32,22 @@ function insertTemplate (file: string, className: string) {
     req.send()
 }
 
-function setTitle () {
+function setHead() {
     const headTitle = document.querySelector("h1")!.innerHTML
     const title = `${headTitle} - ンソピハワールドWiki`
     const head = document.head;
-    head.insertAdjacentHTML("beforeend", `<title>${title}</title>`);
+    head.setAttribute("prefix", "og: https://ogp.me/ns#")
+    head.insertAdjacentHTML("beforeend",
+        `
+        <title>${title}</title>
+        <meta property="og:description" content="epikijetesantakalu Ketaが管理人の非公式Wikiです。">
+        <meta property="og:title" content="${title}">
+        <meta property="og:site_name" content="ンソピハワールドWiki">
+        <meta property="og:image" content="https://epikijetesantakalu.github.io/nsopikha-wiki/images/ogp-image.png">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://epikijetesantakalu.github.io/nsopikha-wiki">
+        <meta name='theme-color' content='#fafb7c'>`
+    );
 }
